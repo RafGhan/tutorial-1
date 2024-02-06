b55ffdefb0462412ad9d27a85504d4355dbf654a
RafGhan <rgharditama@gmail.com>
2024-02-06 21:45:41 +0700

    create delete-product

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -8,4 +8,5 @@ public interface ProductService {
     public List <Product> findAll();
     public Product findById(String id);
     public Product editProduct(Product editedProduct);
+    public Product deleteProduct(String id);
 }
