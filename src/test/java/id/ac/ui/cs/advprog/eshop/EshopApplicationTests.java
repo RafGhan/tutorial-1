e5dbf9e28173d5b9d8b7144564e5124591dda59e
RafGhan <rgharditama@gmail.com>
2024-02-14 18:32:50 +0700

    fixing code smells

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,14 +1,26 @@
 package id.ac.ui.cs.advprog.eshop;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EshopApplicationTests {
 
+	@Autowired
+    private EshopApplication eshopApplication;
+
 	@Test
-	void contextLoads() {
-		EshopApplication.main(new String[] {});
+    void contextLoads() {
+        assertNotNull(eshopApplication);
+    }
+
+	@Test
+	void mainMethodDoesNotThrowException() {
+		assertDoesNotThrow(() -> EshopApplication.main(new String[] {}));
 	}
 
 }
--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class ProductControllerTest {
+class ProductControllerTest {
     @Mock
     private ProductService productService;
     @Mock
